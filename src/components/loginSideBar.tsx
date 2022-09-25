8158df01e5b668c543710f6c1c1f7d9ca2138a22
dorjsurend <dorjoo1015@gmail.com>
2022-09-26 01:02:09 +0800

    fix

--- src/components/loginSideBar.tsx
+++ src/components/loginSideBar.tsx
@@ -14,7 +14,7 @@ export const LoginSideBar = ({ className }: Props) => {
         backgroundPositionX: '100%',
         backgroundRepeat: 'no-repeat',
       }}
-      className={`bg-[url('/assets/images/bg@3x.webp')] h-screen pt-[22px] pl-[28px] relative ${className}`}
+      className={`bg-[url('/assets/images/bg@3x.webp')] min-h-screen pt-[22px] pl-[28px] relative ${className}`}
     >
       <img src="/assets/logos/move-white.svg" className="max-w-[184px]" alt="logo" />
       <div className="flex flex-col">
@@ -30,11 +30,11 @@ export const LoginSideBar = ({ className }: Props) => {
               alt=""
             />
           </div>
-          <div className="relative  xl:-top-[40px]">
+          <div className="relative  xl:-top-[40px] xl:h-[600px] 5xl:h-[710px]">
             <img
               src="/assets/images/group-15@3x.webp"
               className="x:max-w-[500px] xl:max-w-[760px] 5xl:max-w-[900px]"
-              alt="phone "
+              alt="phone"
             />
           </div>
         </div>
