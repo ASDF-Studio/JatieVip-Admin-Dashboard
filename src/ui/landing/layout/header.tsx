7d6cdb90f024012e4a3be0bf76c8fdf7e1a8c0de
dorjsurend <dorjoo1015@gmail.com>
2022-10-06 13:44:52 +0800

    fix

--- src/ui/landing/layout/header.tsx
+++ src/ui/landing/layout/header.tsx
@@ -64,7 +64,7 @@ export const LandingHeader = ({ inView, dark }) => {
           height: 'calc(100vh - 60px)',
           minHeight: ' -webkit-fill-available',
           bottom: '0px',
-          paddingBottom: isSafari && '20px',
+          paddingBottom: isSafari && '40px',
         }}
         className="w-full fixed flex flex-col  bg-white  top-[60px]  overflow-y-auto z-50"
       >
