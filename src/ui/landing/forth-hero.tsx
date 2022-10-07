88bb92e5420ed0f9a512a5b24bd4c68c55137209
dorjsurend <dorjoo1015@gmail.com>
2022-10-07 12:50:14 +0800

    simple mistake

--- src/ui/landing/forth-hero.tsx
+++ src/ui/landing/forth-hero.tsx
@@ -45,8 +45,7 @@ const GetPhone = () => {
 
 export const ForthHero: React.FC = (): React.ReactElement => {
   return (
-    <div id="
-    " className="relative flex bg-white">
+    <div id="habit-tracking" className="relative flex bg-white">
       <GetBg />
 
       <div className="xl:max-w-[302px]  w-full hidden xl:flex" />
