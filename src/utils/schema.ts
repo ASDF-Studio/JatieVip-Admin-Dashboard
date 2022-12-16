678f98c4fdce170920a065c6cffd575c7b087ac2
dorjsurend <dorjoo1015@gmail.com>
2022-12-16 13:53:39 +0800

    update

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,8 +1,16 @@
 import * as yup from 'yup'
 
 export const updateProfileSchema = yup.object({
-  firstName: yup.string().required('required field'),
-  lastName: yup.string().required('required field'),
+  firstName: yup
+    .string()
+    .max(15, 'First name must be at most 15 characters')
+    .matches(/^([A-Za-z])+$/, 'First name must not contain special characters and whitespaces')
+    .required('required field'),
+  lastName: yup
+    .string()
+    .max(15, 'Last name must be at most 15 characters')
+    .matches(/^([A-Za-z])+$/, 'Last name must not contain special characters and whitespaces')
+    .required('required field'),
   birthDay: yup.string().required('required field'),
   gender: yup.string().required('required field'),
 })
@@ -13,5 +21,10 @@ export const loginSchema = yup.object({
 })
 
 export const updateUserNameSchema = yup.object({
-  username: yup.string().trim().max(15).matches(/^\S*$/, "username must not contain whitespaces").required('required field'),
+  username: yup
+    .string()
+    .trim()
+    .max(15)
+    .matches(/^\S*$/, 'username must not contain whitespaces')
+    .required('required field'),
 })
